1ce7ab4887bceeedeb6da76ac3d93fdcf86db593
Haesolla Kim <haesollakim@gmail.com>
2023-12-10 14:26:19 +0900

    master

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-$('svg text').attr('y', '70%');
+$("svg text").attr("y", "70%");
 
 setTimeout(function () {
-    $('.myNameIsHaesollaKim').css({
-        'height': '200px'
+    $(".myNameIsHaesollaKim").css({
+        height: "200px",
     });
-    $('.intro_section').fadeIn(1000);
+    $(".intro_section").fadeIn(1000);
 }, 3000);
 
 setTimeout(function () {
-    $('body').css({
-        'display': 'grid',
-        'grid-template-rows': '200px 1fr'
+    $("body").css({
+        display: "grid",
+        "grid-template-rows": "200px 1fr",
     });
 }, 5000);
