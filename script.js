7b586f487f3f847b1ba4c85dc6f4fde08af569f1
Haesolla Kim <haesollakim@gmail.com>
2023-12-10 14:00:21 +0900

    master

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 $('svg text').attr('y', '70%');
 
-
 setTimeout(function () {
     $('.myNameIsHaesollaKim').css({
         'height': '200px'
     });
-    $('.main_section').fadeIn(1000);
+    $('.intro_section').fadeIn(1000);
 }, 3000);
 
 setTimeout(function () {
@@ -14,47 +13,3 @@ setTimeout(function () {
         'grid-template-rows': '200px 1fr'
     });
 }, 5000);
-
-
-
-
-
-
-
-// // 호버하면 돌아가는 문자
-// const $FPS = 10;
-// const $DURATION = 300;
-// const $CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-// const TEXT = 'PORTFOLIO';
-// const $DELAY = ~~(300 / $FPS);
-// const $FRAME_COUNT = ~~($DURATION / 300) * $FPS
-// const $ELEMENT = document.getElementById('text');
-// let frameIndex = 0;
-// let timeoutId = undefined;
-
-// function resetText() {
-//   if (timeoutId !== undefined) clearTimeout(timeoutId);
-//   frameIndex = 0;
-//   $ELEMENT.innerText = TEXT;
-// }
-
-// function setRandomText() {
-//   const text = Array.from({length: TEXT.length}).map(() => $CHARACTERS[~~(Math.random() * $CHARACTERS.length)]);
-//   $ELEMENT.innerText = text.join('');
-// }
-
-// function animate() {
-//   if (frameIndex >= $FRAME_COUNT) {
-//     resetText();
-//   } else {
-//     frameIndex += 1;
-//     setRandomText();
-//     timeoutId = setTimeout(animate, $DELAY);
-//   }
-// }
-
-// $ELEMENT.addEventListener('mouseenter', animate);
-// $ELEMENT.addEventListener('mouseout', resetText);
-
-// resetText();
-
